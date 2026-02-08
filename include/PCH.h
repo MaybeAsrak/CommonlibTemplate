d7d05483feab8ebe982d8e13827a5e866858ba56
MaybeAsrak <kmanoocheri@gmail.com>
2026-02-08 14:17:35 -0600

    initial version

--- include/PCH.h
+++ include/PCH.h
@@ -2,37 +2,79 @@
 
 #include <RE/Skyrim.h>
 #include <SKSE/SKSE.h>
-
+//#include <FileName.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/msvc_sink.h>
+#include <PerkEntryPointExtenderAPI.h>
 
 #define ERROR(message, ...) SKSE::log::error(message, ##__VA_ARGS__)
 #define INFO(message, ...) SKSE::log::info(message, ##__VA_ARGS__)
 #define TRACE(message, ...) SKSE::log::trace(message, ##__VA_ARGS__)
 #define WARN(message, ...) SKSE::log::warn(message, ##__VA_ARGS__)
+namespace logger = SKSE::log;
 
-namespace stl
-{ 
+namespace stl {
     using namespace SKSE::stl;
-
-    template <class T, std::size_t index, class U>
-    void write_vfunc()
-    {
-       REL::Relocation VTABLE{ T::VTABLE[0] };
-       U::Callback = VTABLE.write_vfunc(index, U::Call);
-    }
-   
-    template <class T>
-    void write_vfunc_call(std::uintptr_t a_source)
-    {
-        auto& trampoline = SKSE::GetTrampoline();
-        T::Callback = *reinterpret_cast<std::uintptr_t*>(trampoline.write_call<6>(a_source, T::Call));
+    template <std::integral T, std::size_t N>
+    void safe_write(std::uintptr_t a_dst, const std::array<T, N>& a_data) {
+        REL::safe_write(a_dst, a_data.data(), a_data.size() * sizeof(T));
     }
 
     template <class T>
-    void write_thunk_call(std::uintptr_t a_source)
-    {
+    void write_call(std::uintptr_t a_source) {
+        SKSE::AllocTrampoline(14);
         auto& trampoline = SKSE::GetTrampoline();
-        T::Callback = trampoline.write_call<5>(a_source, T::Call);
+        trampoline.write_call<5>(a_source, T::Call);
+    }
+    template <class T>
+    void write_thunk_call(std::uintptr_t a_src) {
+        SKSE::AllocTrampoline(14);
+
+        auto& trampoline = SKSE::GetTrampoline();
+        T::func = trampoline.write_call<5>(a_src, T::thunk);
+    }
+
+    //template <class F, std::size_t idx, class T>
+    //void write_vfunc() {
+    //    REL::Relocation<std::uintptr_t> vtbl{F::VTABLE[0]};
+    //    T::func = vtbl.write_vfunc(idx, T::thunk);
+    //}
+    template <class F, std::size_t idx, class T, std::size_t x = 0>
+    void write_vfunc() {
+        REL::Relocation<std::uintptr_t> vtbl{F::VTABLE[x]};
+        T::func = vtbl.write_vfunc(idx, T::thunk);
+    }
+    template <std::size_t idx, class T>
+    void write_vfunc(REL::VariantID id) {
+        REL::Relocation<std::uintptr_t> vtbl{id};
+        T::func = vtbl.write_vfunc(idx, T::thunk);
     }
 }
+
+
+
+//namespace stl
+//{ 
+//    using namespace SKSE::stl;
+//
+//    template <class T, std::size_t index, class U>
+//    void write_vfunc()
+//    {
+//       REL::Relocation VTABLE{ T::VTABLE[0] };
+//       U::Callback = VTABLE.write_vfunc(index, U::Call);
+//    }
+//   
+//    template <class T>
+//    void write_vfunc_call(std::uintptr_t a_source)
+//    {
+//        auto& trampoline = SKSE::GetTrampoline();
+//        T::Callback = *reinterpret_cast<std::uintptr_t*>(trampoline.write_call<6>(a_source, T::Call));
+//    }
+//
+//    template <class T>
+//    void write_thunk_call(std::uintptr_t a_source)
+//    {
+//        auto& trampoline = SKSE::GetTrampoline();
+//        T::Callback = trampoline.write_call<5>(a_source, T::Call);
+//    }
+//}
--- source/Main.cpp
+++ source/Main.cpp
@@ -1,3 +1,97 @@
+#include <PerkEntryPointExtenderAPI.h>
+
+
+
+
+
+
+
+
+
+//Mod 1:
+//
+//Spell fusion
+//
+//spell + spell (+ shout)
+//
+//spell + keyword (+ shout)
+//
+//spell + spell (+ shout keyword?)
+//
+//create a map that is {spell to cast, spell 1, spell 2, shout*} or one that is {spell to cast, spell 1, keyword 2, shout*} and then you iterate through and reorder for priority?
+//
+//need to get hooks for unlocked dual casting, override spell, override dual cast, override cost 
+//
+//Shout could control dual casting or modifier benefits
+
+
+
+
+
+struct Hooks {
+    struct DualCastPerkCheck {
+        static void thunk(void* entry_point, RE::Actor* caster, RE::MagicItem* a_spell, void* outbound) {
+            //bool testbool;
+            //func(entry_point, caster, a_spell, &outbound);
+            //if (testbool == true) {
+            //    if (caster->GetActorRuntimeData().selectedSpells[0] == caster->GetActorRuntimeData().selectedSpells[1]) {
+            //        *outbound = true;
+            //    }
+            //    else {
+            //        float dualcast = 0.0f;
+            //        RE::HandleEntryPoint(RE::PerkEntryPoint::kCanDualCastSpell, caster, &dualcast, "DCO", 3, {a_spell});
+            //        if (dualcast > 0.0f) {
+            //            //*outbound = true; 
+            //            bool* boolPtr = static_cast<bool*>(outbound);
+            //*boolPtr = true; 
+            //        }
+            //        else {
+            float testfloat = 0.0f;
+            RE::BGSEntryPoint::HandleEntryPoint(RE::BGSPerkEntry::EntryPoint::kCanDualCastSpell, caster, a_spell, &testfloat);
+                        //func(entry_point, caster, a_spell, &testfloat);
+                        if (testfloat == 0.0f) {
+
+                            bool* boolPtr = static_cast<bool*>(outbound);
+                            *boolPtr = true;
+                        }
+                        //*outbound = false; // could have an additional failsafe here?
+                    //}
+            //    }
+
+
+            //}
+
+        }
+
+        static inline REL::Relocation<decltype(thunk)> func;
+    };
+
+
+
+
+	static void Install() {
+
+		if SKYRIM_REL_CONSTEXPR(REL::Module::IsAE()) {
+            INFO("Hello, AE user {}!", SKSE::PluginDeclaration::GetSingleton()->GetAuthor());
+			REL::Relocation<std::uintptr_t> targetDual{RELOCATION_ID(0, 38762), REL::Relocate(0x0, 0x92)};
+			REL::safe_fill(targetDual.address(), REL::NOP, 0xD);
+            REL::Relocation<std::uintptr_t> DualCastCheckHookB{RELOCATION_ID(0, 38762), REL::Relocate(0x0, 0xDA)};
+        stl::write_thunk_call<DualCastPerkCheck>(DualCastCheckHookB.address());
+
+		}
+        REL::Relocation<std::uintptr_t> DualCastSpellCheck{RELOCATION_ID(37816, 38765), REL::Relocate(0x8D, 0x8D)};
+        REL::safe_fill(DualCastSpellCheck.address(), REL::NOP, 0x9);
+
+        REL::Relocation<std::uintptr_t> DualCastCheckHookA{RELOCATION_ID(37816, 38765), REL::Relocate(0xCD, 0xCD)};
+        stl::write_thunk_call<DualCastPerkCheck>(DualCastCheckHookA.address());
+
+
+
+
+	}
+};
+
+
 void InitializeLogger()
 {
     auto path = SKSE::log::log_directory();
@@ -27,6 +121,7 @@ void HandleMessage(SKSE::MessagingInterface::Message* a_message)
     case SKSE::MessagingInterface::kDataLoaded:
         {
             INFO("Hello, {}!", SKSE::PluginDeclaration::GetSingleton()->GetAuthor());
+            Hooks::Install();
         }
         break;
     }
