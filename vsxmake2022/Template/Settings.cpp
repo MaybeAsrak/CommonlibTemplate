e93007a7ea8c54dedb562bbd3f6882fc99e50adc
MaybeAsrak <kmanoocheri@gmail.com>
2026-03-21 17:07:07 -0500

    wip

---
+++ vsxmake2022/Template/Settings.cpp
@@ -0,0 +1,35 @@
+#include "Settings.h"
+
+void Settings::Load()
+{
+	// Messagebox
+
+
+
+	try {
+		//const auto settings = toml::parse(TOML_PATH);
+		const auto settings = toml::parse(TOML_PATH);
+
+		ToggleHotkey1 = toml::find<int>(settings, "ToggleHotkey1");
+
+
+
+	}
+	catch ([[maybe_unused]] const toml::syntax_error& error) {
+		// SKSE::stl::report_and_error(std::format("Settings::Load ~ Failed to parse TOML! <{}>", error.what()));
+		stl::report_and_error(std::format("Settings::Load ~ Failed to parse TOML! <{}>", error.what()));
+
+	}
+}
+
+Settings::Settings() : toggle(true) {}
+
+bool Settings::GetToggle() const
+{
+	return toggle;
+}
+
+void Settings::SetToggle(bool a_value)
+{
+	toggle = a_value;
+}
